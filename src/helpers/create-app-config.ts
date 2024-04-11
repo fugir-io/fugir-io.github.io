db0cc6d731e3bb9ea0a72408cb5904af5fb7e1e3
JB <joseph.barnett@gmail.com>
2024-04-11 13:41:36 -0300

    document helpers

--- src/helpers/create-app-config.ts
+++ src/helpers/create-app-config.ts
@@ -1,20 +1,29 @@
-export type AppConfig = {
-  title: string;
+/**
+ * @fileoverview Helpers for Creating Application Configuration
+ * This file contains helper functions for creating application configurations.
+ * It defines the AppConfig type representing the configuration options for an application.
+ * Additionally, it provides the createAppConfig function to create a complete application configuration.
+ */
 
-  resizable?: boolean;
-  expandable?: boolean;
-  height?: string | number;
-  width?: string | number;
+/**
+ * Type representing the configuration options for an application.
+ * @typedef {object} AppConfig
+ * @property {string} title The title of the application.
+ * @property {boolean} [resizable=true] Whether the application window is resizable.
+ * @property {boolean} [expandable=false] Whether the application window is expandable.
+ * @property {string|number} [height='500'] The height of the application window.
+ * @property {string|number} [width='600'] The width of the application window.
+ * @property {boolean} [shouldOpenWindow=true] Whether the application window should open by default.
+ * @property {(e: unknown) => void} [externalAction] The action to perform when the dock button is clicked.
+ * @property {boolean} [dockBreaksBefore=false] Whether there should be a break in the dock before this app.
+ */
 
-  shouldOpenWindow?: boolean;
-
-  /** The action to do when dock button is clicked */
-  externalAction?: (e: unknown) => void;
-
-  /** Break before this app */
-  dockBreaksBefore?: boolean;
-};
-
+/**
+ * Function to create a complete application configuration with default values.
+ * It takes an AppConfig object as input and merges it with default configuration values.
+ * @param {AppConfig} et The partial application configuration.
+ * @returns {AppConfig} The complete application configuration.
+ */
 export const createAppConfig = (et: AppConfig) => ({
   shouldOpenWindow: true,
   dockBreaksBefore: false,
--- src/helpers/wait-for.ts
+++ src/helpers/wait-for.ts
@@ -1,1 +1,8 @@
-export const waitFor = (time: number) => new Promise((res) => setTimeout(res, time));
+/**
+ * @fileoverview Utility Function for Waiting
+ * This file contains a utility function for waiting for a specified amount of time.
+ *
+ * @param {number} time The time to wait in milliseconds.
+ * @returns {Promise<void>} A promise that resolves after the specified time has elapsed.
+ */
+export const waitFor = (time: number): Promise<void> => new Promise((res) => setTimeout(res, time));
